c2dd153f896e8ece1dc1edc5849271bf33fcbc72
许文龙 <fickxu@163.com>
2020-06-20 21:25:06 +0800

    新增投保详情；入库列表

---
+++ pages/insured-info/insured-info.js
@@ -0,0 +1,19 @@
+import request from '../api/request'
+
+const app = getApp();
+// pages/home/home.js
+Page({
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title: '投保信息',
+  },
+  onLoad() {
+  },
+  // 输入要求
+  textareaBInput(e) {}
+})
