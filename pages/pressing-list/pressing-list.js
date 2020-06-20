284a88961bc839b79eae2b33860df9f3c3de3391
许文龙 <fickxu@163.com>
2020-06-20 21:55:08 +0800

    新增压料列表

---
+++ pages/pressing-list/pressing-list.js
@@ -0,0 +1,17 @@
+import request from '../api/request'
+
+const app = getApp();
+// pages/home/home.js
+Page({
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title: '压料列表',
+  },
+  onLoad() {
+  }
+})
