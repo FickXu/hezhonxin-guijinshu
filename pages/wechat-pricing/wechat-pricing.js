db823be3d742be1293f16ecf34fcbb45543fcdfb
许文龙 <fickxu@163.com>
2020-06-21 15:03:16 +0800

    新增微信定价页面

---
+++ pages/wechat-pricing/wechat-pricing.js
@@ -0,0 +1,30 @@
+import request from '../api/request'
+
+const app = getApp();
+// pages/home/home.js
+Page({
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title: '微信定价',
+    date: '2020-6-20',
+    date1: '2020-6-20'
+  },
+  onLoad() {
+  },
+  DateChange(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  DateChange1(e) {
+    this.setData({
+      date1: e.detail.value
+    })
+  },
+  textareaBInput() {}
+})
